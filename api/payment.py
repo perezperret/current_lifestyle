b155eb2971f8580f1f5aadf61a9a230228ebc938
Santiago Perez Perret <perezperret@gmail.com>
2020-08-26 17:18:39 -0300

    Re-add functionality

--- api/payment.py
+++ api/payment.py
@@ -1,37 +1,37 @@
-# import numpy_financial as npf
-#
-# def current_lifestyle_pmt(
-#     current_monthly_expenses,
-#     annual_inflation,
-#     saving_years,
-#     saving_annual_return,
-#     retirement_years,
-#     retirement_annual_return,
-#     initial_investment):
-#
-#     retirement_monthly_return = ((1 + retirement_annual_return) ** (1 / 12)) - 1
-#     saving_monthly_return = ((1 + saving_annual_return) ** (1 / 12)) - 1
-#     monthly_inflation = ((1 + annual_inflation) ** (1 / 12)) - 1
-#
-#     real_retirement_pmt = npf.fv(
-#         rate=monthly_inflation, pv=-2000, nper=(saving_years * 12), pmt=0)
-#
-#     retirement_fund = npf.pv(
-#         rate=retirement_monthly_return-monthly_inflation,
-#         nper=(retirement_years * 12),
-#         pmt=real_retirement_pmt)
-#
-#     saving_payment = npf.pmt(
-#         rate=saving_period_monthly_return-monthly_inflation,
-#         nper=(saving_years * 12),
-#         pv=initial_investment,
-#         fv=retirement_fund)
-#
-#     return {
-#         'real_retirement_pmt': real_retirement_pmt,
-#         'retirement_fund': -retirement_fund,
-#         'saving_payment': saving_payment
-#     }
+import numpy_financial as npf
+
+def current_lifestyle_pmt(
+    current_monthly_expenses,
+    annual_inflation,
+    saving_years,
+    saving_annual_return,
+    retirement_years,
+    retirement_annual_return,
+    initial_investment):
+
+    retirement_monthly_return = ((1 + retirement_annual_return) ** (1 / 12)) - 1
+    saving_monthly_return = ((1 + saving_annual_return) ** (1 / 12)) - 1
+    monthly_inflation = ((1 + annual_inflation) ** (1 / 12)) - 1
+
+    real_retirement_pmt = npf.fv(
+        rate=monthly_inflation, pv=-2000, nper=(saving_years * 12), pmt=0)
+
+    retirement_fund = npf.pv(
+        rate=retirement_monthly_return-monthly_inflation,
+        nper=(retirement_years * 12),
+        pmt=real_retirement_pmt)
+
+    saving_payment = npf.pmt(
+        rate=saving_period_monthly_return-monthly_inflation,
+        nper=(saving_years * 12),
+        pv=initial_investment,
+        fv=retirement_fund)
+
+    return {
+        'real_retirement_pmt': real_retirement_pmt,
+        'retirement_fund': -retirement_fund,
+        'saving_payment': saving_payment
+    }
 
 import json
 from http.server import BaseHTTPRequestHandler
@@ -43,14 +43,14 @@ class handler(BaseHTTPRequestHandler):
         self.send_header('Content-type','application/json')
         self.end_headers()
 
-        # clpmt = current_lifestyle_pmt(
-        #     current_monthly_expenses = 2000,
-        #     annual_inflation = 0.02,
-        #     saving_years = 35,
-        #     saving_annual_return = 0.05,
-        #     retirement_years = 25,
-        #     retirement_annual_return = 0.03,
-        #     initial_investment = 0)
+        clpmt = current_lifestyle_pmt(
+            current_monthly_expenses = 2000,
+            annual_inflation = 0.02,
+            saving_years = 35,
+            saving_annual_return = 0.05,
+            retirement_years = 25,
+            retirement_annual_return = 0.03,
+            initial_investment = 0)
 
-        self.wfile.write(json.dumps({ 'hello': 'world' }).encode())
+        self.wfile.write(json.dumps(clpmt).encode())
         return
