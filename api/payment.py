cef7c5ba405310b729a03b2ba0469df5890217ab
Santiago Perez Perret <perezperret@gmail.com>
2020-08-27 10:24:37 -0300

    Use Flask to clean up payment function

--- api/payment.py
+++ api/payment.py
@@ -1,26 +1,17 @@
-import json
-import urllib.parse
-from http.server import BaseHTTPRequestHandler
+from flask import Flask, Response, request, jsonify
 from lib.calculator import current_lifestyle_pmt
 
-class handler(BaseHTTPRequestHandler):
+app = Flask(__name__)
 
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','application/json')
-        self.end_headers()
+@app.route('/api/payment')
+def get():
+    clpmt = current_lifestyle_pmt(
+        current_monthly_expenses = float(request.args.get('current_monthly_expenses')),
+        annual_inflation = float(request.args.get('annual_inflation')) / 100,
+        saving_years = int(request.args.get('saving_years')),
+        saving_annual_return = float(request.args.get('saving_annual_return')) / 100,
+        retirement_years = int(request.args.get('retirement_years')),
+        retirement_annual_return = float(request.args.get('retirement_annual_return')) / 100,
+        initial_investment = float(request.args.get('initial_investment')))
 
-        url = urllib.parse.urlparse(self.path)
-        query_params = urllib.parse.parse_qs(url.query)
-
-        clpmt = current_lifestyle_pmt(
-            current_monthly_expenses = float(query_params['current_monthly_expenses'][0]),
-            annual_inflation = float(query_params['annual_inflation'][0]) / 100,
-            saving_years = int(query_params['saving_years'][0]),
-            saving_annual_return = float(query_params['saving_annual_return'][0]) / 100,
-            retirement_years = int(query_params['retirement_years'][0]),
-            retirement_annual_return = float(query_params['retirement_annual_return'][0]) / 100,
-            initial_investment = float(query_params['initial_investment'][0]))
-
-        self.wfile.write(json.dumps(clpmt).encode())
-        return
+    return jsonify(clpmt)
