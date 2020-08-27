e61d03a021248aaa221a7a12370aa461609a034a
Santiago Perez Perret <perezperret@gmail.com>
2020-08-27 19:06:53 -0300

    Re-design and improve styling

--- public/script.js
+++ public/script.js
@@ -10,10 +10,10 @@ document.getElementById('payment-form').addEventListener('submit', (e) => {
         style: 'currency', currency: 'USD'
       }).format
       preview.style.display = 'block'
-      preview.innerText = [
-        `You would need a monthly payout equivalent to USD ${format(result.real_retirement_pmt)} during retirement.`,
-        `You would need a fund with USD ${format(result.retirement_fund)}.`,
-        `You should pay in USD ${format(result.saving_payment)} every month.`
+      preview.innerHTML = [
+        `<p>You would need a monthly payout of <strong>${format(result.real_retirement_pmt)}</strong> during retirement.</p>`,
+        `<p>You would need a fund with <strong>${format(result.retirement_fund)}</strong> by the time you retire.</p>`,
+        `<p>You should save <strong>${format(result.saving_payment)}</strong> for retirement every month.</p>`
       ].join("\n\n")
     })
 })
