2d49528952e4b73895c160d112db450f006ca696
Santiago Perez Perret <perezperret@gmail.com>
2020-08-27 10:32:50 -0300

    Simplify things by directly posting the form

--- api/payment.py
+++ api/payment.py
@@ -3,15 +3,15 @@ from lib.calculator import current_lifestyle_pmt
 
 app = Flask(__name__)
 
-@app.route('/api/payment')
+@app.route('/api/payment', methods=['POST'])
 def get():
     clpmt = current_lifestyle_pmt(
-        current_monthly_expenses = float(request.args.get('current_monthly_expenses')),
-        annual_inflation = float(request.args.get('annual_inflation')) / 100,
-        saving_years = int(request.args.get('saving_years')),
-        saving_annual_return = float(request.args.get('saving_annual_return')) / 100,
-        retirement_years = int(request.args.get('retirement_years')),
-        retirement_annual_return = float(request.args.get('retirement_annual_return')) / 100,
-        initial_investment = float(request.args.get('initial_investment')))
+        current_monthly_expenses = float(request.form['current_monthly_expenses']),
+        annual_inflation = float(request.form['annual_inflation']) / 100,
+        saving_years = int(request.form['saving_years']),
+        saving_annual_return = float(request.form['saving_annual_return']) / 100,
+        retirement_years = int(request.form['retirement_years']),
+        retirement_annual_return = float(request.form['retirement_annual_return']) / 100,
+        initial_investment = float(request.form['initial_investment']))
 
     return jsonify(clpmt)
--- public/script.js
+++ public/script.js
@@ -1,17 +1,8 @@
 document.getElementById('payment-form').addEventListener('submit', (e) => {
   e.preventDefault()
+  const form = e.target
 
-  values = []
-  formData = new FormData(e.target)
-  for (var key of formData.keys()) {
-    values.push([key, formData.get(key)])
-  }
-
-  query = values.map(([value, name]) => {
-    if (value && name) { return `${value}=${name}` }
-  }).filter(i => i).join('&');
-
-  fetch(`${e.target.action}?${query}`)
+  fetch(form.action, { method: form.method, body: new FormData(form) })
     .then(response => response.json())
     .then(result => {
       const preview = document.getElementById('result-preview')
